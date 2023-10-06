2b0ef498d72931cacd34b86b289958483e6438c5
BLUE <hoanghieu25102@gmail.com>
2023-10-06 09:58:55 +0700

    nothing

--- index.ts
+++ index.ts
@@ -1,6 +1,6 @@
 
 // write your NFT miner here
-import {Address, TonClient} from "ton"
+import {Address, toNano, TonClient} from "ton"
 import BN from "bn.js";
 import {unixNow} from "./src/lib/utils";
 import {MineMessageParams, Queries} from "./src/giver/NftGiver.data";
@@ -40,15 +40,52 @@ async function main () {
         seed // unique seed from get_mining_data
     };
 
-    let msg = Queries.mine(mineParams); // transaction builder
+    let msg = Queries.mine(mineParams);
+    let progress = 0;
 
     while (new BN(msg.hash(), 'be').gt(complexity)) {
+        progress += 1
+        console.clear()
+        console.log(`Mining started: please, wait for 30-60 seconds to mine your NFT!`)
+        console.log(' ')
+        console.log(`⛏ Mined ${progress} hashes! Last: `, new BN(msg.hash(), 'be').toString())
+
         mineParams.expire = unixNow() + 300
         mineParams.data1.iaddn(1)
         msg = Queries.mine(mineParams)
     }
 
-    console.log('Yoo-hoo, you found something!')
+    console.log(' ')
+    console.log('💎 Mission completed: msg_hash less than pow_complexity found!');
+    console.log(' ')
+    console.log('msg_hash: ', new BN(msg.hash(), 'be').toString())
+    console.log('pow_complexity: ', complexity.toString())
+    console.log('msg_hash < pow_complexity: ', new BN(msg.hash(), 'be').lt(complexity))
+
+    console.log(' ');
+    console.log("💣 WARNING! As soon as you find the hash, you should quickly send the transaction.");
+    console.log("If someone else sends a transaction before you, the seed changes, and you'll have to find the hash again!");
+    console.log(' ');
+
+    // flags work only in user-friendly address form
+    const collectionAddr = collection.toFriendly({
+        urlSafe: true,
+        bounceable: true,
+    })
+    // we must convert TON to nanoTON
+    const amountToSend = toNano('0.05').toString()
+    // BOC means Bag Of Cells here
+    const preparedBodyCell = msg.toBoc().toString('base64url')
+
+    // final method to build a payment URL
+    const tonDeepLink = (address: string, amount: string, body: string) => {
+        return `ton://transfer/${address}?amount=${amount}&bin=${body}`;
+    };
+
+    const link = tonDeepLink(collectionAddr, amountToSend, preparedBodyCell);
+
+    console.log('🚀 Link to receive an NFT:')
+    console.log(link);
 }
 
 main()
